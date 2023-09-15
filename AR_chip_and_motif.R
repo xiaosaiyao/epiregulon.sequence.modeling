9b0ba24289ff81c9632ca1933f7fab6f75c2b2c3
Toneyan <toneyans@gene.com>
2023-09-15 09:52:46 -0700

    add ROC plots

--- AR_chip_and_motif.R
+++ AR_chip_and_motif.R
@@ -7,11 +7,26 @@ library(eulerr)
 library(ggpubr)
 library(dplyr)
 library(glue)
-source('./utils.R')
+
+
+quantile_probability_thresholds = c(0.005, 0.05, 0.1, 0.2, 0.3, 0.5, 0.6, 0.75)
+drug = 'Enza'
+motif_name = 'AR_689'
+celllines <- c("LNCaP", "VCaP")
+model_name = 'basenji'
+
+
+base_dir = dirname(rstudioapi::getActiveDocumentContext()$path)
+source(glue('{base_dir}/utils.R'))
+
+reg_dir = glue('{base_dir}/regulons/')
+dir.create(reg_dir)
+plot_dir = glue("{base_dir}/ROC_plots/")
+dir.create(plot_dir)
 
 #checkdata
 data_dir = '/gstore/project/ar_ligands/AR/scRNAseq/nonpipeline/'
-archR_project_path <- "/gstore/project/ar_ligands/AR/scRNAseq/nonpipeline/OUTPUT/ArchRProject"
+archR_project_path <- glue("{data_dir}/OUTPUT/ArchRProject")
 proj.all <- loadArchRProject(path = archR_project_path, showLogo = TRUE)
 
 proj.all <- proj.all[!proj.all$hash_assignment2 %in% c(paste0("SAM24425416HTO-",14:16),
@@ -23,27 +38,14 @@ proj.all <- proj.all[!proj.all$hash_assignment2 %in% c(paste0("SAM24425416HTO-",
 
 extraCols_narrowPeak <- c(singnalValue = "numeric", pValue = "numeric",
                           qValue = "numeric", peak = "integer")
-# grl[["LNCaP"]][["AR"]] <- rtracklayer::import(glue("{data_dir}/data/SRX2017926.05.bed"), extraCols = extraCols_narrowPeak) #GSM2277148
-# grl[["VCaP"]][["AR"]] <- rtracklayer::import(glue("{data_dir}/data/SRX471847.05.bed"), extraCols = extraCols_narrowPeak) #GSM1328945
 
-motif_name = 'AR_689'
-base_dir = '~/tf_re_mapping/'
+
 motif_positions_all = readRDS('/gstore/project/ar_ligands/AR/scRNAseq/nonpipeline/OUTPUT/ArchRProject/Annotations/Motif-Positions-In-Peaks.rds')
 motif_positions = motif_positions_all[[motif_name]]
-motif_positions
-write.csv(motif_positions, glue('{base_dir}/{motif_name}.csv')) # save for running CBP predictions
 
 grl_public <- getTFMotifInfo(genome = "hg38")
 
-# grl[["LNCaP"]][["FOXA1"]] <- rtracklayer::import(glue("{data_dir}/data/SRX1280790.05.bed"), extraCols = extraCols_narrowPeak) #GSM1891830
-# grl[["VCaP"]][["FOXA1"]] <- rtracklayer::import(glue("{data_dir}/data/SRX2642364.05.bed"), extraCols = extraCols_narrowPeak)  #GSM2537225
-# grl[["22Rv1"]][["FOXA1"]] <- rtracklayer::import(glue("{data_dir}/data/SRX5124035.05.bed"), extraCols = extraCols_narrowPeak)  #GSM3507235
-# grl[["MDA"]][["FOXA1"]] <- rtracklayer::import("/gstore/project/ar_ligands/AR/chip/LAB11158_A9690/OUTPUT/diffbind/diffbind.allFOXA1.DBA_DESEQ2.bed")
-# 
-# grl[["MDA"]][["SMARCA4"]] <- rtracklayer::import("/gstore/project/ar_ligands/AR/chip/LAB11158_A9690/OUTPUT/diffbind/diffbind.allSMARCA4.DBA_DESEQ2.bed")
 
-
-celllines <- c("LNCaP", "VCaP")
 
 
 
@@ -94,16 +96,19 @@ names(assays(peakMatrix)) <- "counts"
 cell_bed_mapping = list(LNCaP='SRX2017926.05', 
                         VCaP='SRX471847.05')
 
-reg_dir = '~/tf_re_mapping/regulons_fin/'
-dir.create(reg_dir)
-model_name = 'basenji'
+
 
 
 
 
 for (cell in celllines){
+    score_file = glue("{base_dir}/scores/{model_name}_{motif_name}_{cell}.csv")
+    if (file.exists(score_file)){
+      dataset_all <- read.csv(score_file)
+      thresholds = quantile(dataset_all$count, probs=quantile_probability_thresholds)
+      
+    }
     
-    dataset_all <- read.csv(glue("{base_dir}/{model_name}_scores_{motif_name}_all.csv"))
   
     message(cell)
     # pick cell line specific ChIP
@@ -116,7 +121,6 @@ for (cell in celllines){
     o = findOverlaps(grl_public$AR, motif_positions)
     grl_public_chip_in_motifs = grl_public$AR[unique(queryHits(o)),]
     # threshold DNN scores
-    thresholds = quantile(dataset_all$count, probs=c(0.005, 0.05, 0.1, 0.2, 0.3, 0.5, 0.6, 0.75))
 
 
     tf_mappings = list(public_chip=grl_public$AR, # public AR ChIP
@@ -124,17 +128,17 @@ for (cell in celllines){
                      motif=motif_positions, # motif ChIP
                      chip_in_motif=grl_chip_in_motifs, # ChIP overlapping motif
                      public_chip_in_motifs=grl_public_chip_in_motifs)
-
-    for (t_i in thresholds){
-      dataset = dataset_all[dataset_all$count>t_i,]
+    if (file.exists(score_file)){
+      for (t_i in thresholds){
+        dataset = dataset_all[dataset_all$count>t_i,]
+        
+        print(length(dataset$seqnames))
+        cbp_filtered <- GRanges(seqnames = Rle(dataset$seqnames),
+                                ranges = IRanges(dataset$start, dataset$end),
+                                strand = Rle("*"))
+        tf_mappings[[glue('{model_name} {signif(t_i, 3)}')]] = cbp_filtered
+      }}
       
-      print(length(dataset$seqnames))
-      cbp_filtered <- GRanges(seqnames = Rle(dataset$seqnames),
-                              ranges = IRanges(dataset$start, dataset$end),
-                              strand = Rle("*"))
-      tf_mappings[[glue('{model_name}-{signif(t_i, 3)}')]] = cbp_filtered
-    }
-    
     
     selected <- which(proj.all$Cell == cell )
     proj <- proj.all[selected,]
@@ -210,8 +214,8 @@ for (cell in celllines){
                                        mode = "weight",
                                        FUN = "mean")
       res = calculate_ROC(regulon.w, 'AR', 'DMSO', GeneExpressionMatrix.select,
-                          label_column='TEST_ARTICLE', negative_group_label='Enza',
-                          scale_expression=FALSE, exp_assay = "normalizedCounts")
+                          label_column='TEST_ARTICLE', negative_group_label=drug,
+                          exp_assay = "normalizedCounts")
       roc_w = signif(calculate_AUC(res$accuracy$FPR, res$accuracy$TPR), 3)
       n_TG = length((unique(pruned.regulon$target)))
       dfs_weighted[[label]] <- data.frame('FPR'=res$accuracy$FPR, 
@@ -223,7 +227,7 @@ for (cell in celllines){
       regulon.noweights$weight = 1
       res = calculate_ROC(regulon.noweights, 'AR', 'DMSO', GeneExpressionMatrix.select,
                           label_column='TEST_ARTICLE', negative_group_label='Enza',
-                          scale_expression=FALSE, exp_assay = "normalizedCounts")
+                          exp_assay = "normalizedCounts")
       roc_n = signif(calculate_AUC(res$accuracy$FPR, res$accuracy$TPR), 3)
       
       dfs_noweight[[label]] <- data.frame('FPR'=res$accuracy$FPR, 
@@ -231,8 +235,7 @@ for (cell in celllines){
                                           'set'=rep(glue('{label} {n_TG} {roc_n}'), length(res$accuracy$FPR)))
       
     dfs_weighted_all = bind_rows(dfs_weighted) 
-    plot_dir = glue("~/tf_re_mapping/plots_fin/")
-    dir.create(plot_dir)
+
     
     pdf(glue("{plot_dir}/AR_ROC_plots_{cell}.pdf"))
     
@@ -253,35 +256,27 @@ for (cell in celllines){
 
 
 
-# plot Venn diagrams
-library(VennDiagram)
-cell = 'LNCaP'
-file_prefix = glue("{reg_dir}/{cell}_")
-
-col_to_venn = 'idxATAC'
-venn_diagram_info = list()
-regulons = list()
-for (reg_type in c('chip', 'public_chip')){
-  reg = read.csv(glue('{file_prefix}{reg_type}.csv'))
-  venn_diagram_info[[reg_type]] = reg[[col_to_venn]]
-  
-}
-
-
-
-# for (i in names(regulons)){
-#   if (!(grepl('_', i, fixed=TRUE))){
-#     venn_diagram_info[[i]] = regulons[[i]][[col_to_venn]]
-#   }
+# # plot Venn diagrams
+# library(VennDiagram)
+# cell = 'LNCaP'
+# file_prefix = glue("{reg_dir}/{cell}_")
+# 
+# col_to_venn = 'idxATAC'
+# venn_diagram_info = list()
+# regulons = list()
+# for (reg_type in c('chip', 'public_chip')){
+#   reg = read.csv(glue('{file_prefix}{reg_type}.csv'))
+#   venn_diagram_info[[reg_type]] = reg[[col_to_venn]]
 #   
 # }
-
-temp <- venn.diagram(venn_diagram_info,
-                     # fill = c("red", "blue"), alpha = c(0.5, 0.5),
-                     cex = 2, main=glue('{col_to_venn} {cell}'),
-                    filename = NULL)
-dev.off(dev.list()["RStudioGD"])
-grid.draw(temp)
+# 
+# 
+# temp <- venn.diagram(venn_diagram_info,
+#                      # fill = c("red", "blue"), alpha = c(0.5, 0.5),
+#                      cex = 2, main=glue('{col_to_venn} {cell}'),
+#                     filename = NULL)
+# dev.off(dev.list()["RStudioGD"])
+# grid.draw(temp)
 
 
 
